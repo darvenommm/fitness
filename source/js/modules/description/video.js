969c5d80f6aeb56e2ccdf52109ab1a9f12d22761
Denis Romodanov <darvenommm@gmail.com>
2023-07-02 22:47:35 +0300

    feat: add description block

---
+++ source/js/modules/description/video.js
@@ -0,0 +1,28 @@
+const HAS_VIDEO_CLASS = 'description__video--has-video';
+
+const root = document.querySelector('.description__video');
+const button = root.querySelector('button');
+
+export const addVideoHandlers = () => {
+  button.addEventListener('click', () => {
+    root.replaceChildren();
+    root.classList.add(HAS_VIDEO_CLASS);
+    root.innerHTML = `
+      <iframe
+        width="364"
+        height: "228"
+        src="${button.dataset.src}"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer;
+        autoplay;
+        clipboard-write;
+        encrypted-media;
+        gyroscope;
+        picture-in-picture;
+        web-share"
+        allowfullscreen
+      ></iframe>
+    `;
+  }, {once: true});
+};
