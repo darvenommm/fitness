f0d8a70f571919a72fe491883b7bf50798577205
Denis Romodanov <darvenommm@gmail.com>
2023-07-07 22:09:23 +0300

    feat: stock block

--- source/js/modules/subscriptions/card.js
+++ source/js/modules/subscriptions/card.js
@@ -1,10 +1,13 @@
 export const addButtonsClassByCardHover = () => {
+  const ROOT_SELECTOR = '.subscriptions';
+  const TAB_SELECTOR = '.subscriptions__button';
   const ACTIVE_BUTTON_CLASS = 'active';
   const ACTIVE_CARD_CLASS = 'active';
   const CARD_SELECTOR = '.subscriptions__item';
   const BUTTON_SELECTOR = '.subscriptions__buy-button';
 
-  const cards = document.querySelectorAll(CARD_SELECTOR);
+  const root = document.querySelector(ROOT_SELECTOR);
+  const cards = root.querySelectorAll(CARD_SELECTOR);
 
   // for mobile
   // else for desktop
@@ -27,7 +30,6 @@ export const addButtonsClassByCardHover = () => {
   } else {
     cards.forEach((card) => {
       // !!! card's active class activates at css code by hover pseudoclass
-
       const button = card.querySelector(BUTTON_SELECTOR);
 
       card.addEventListener('mouseover', () => {
@@ -36,6 +38,26 @@ export const addButtonsClassByCardHover = () => {
 
       card.addEventListener('mouseout', () => {
         button.classList.remove(ACTIVE_BUTTON_CLASS);
+      });
+
+      button.addEventListener('focus', () => {
+        card.classList.add(ACTIVE_CARD_CLASS);
+      });
+
+      button.addEventListener('blur', () => {
+        card.classList.remove(ACTIVE_CARD_CLASS);
+      });
+    });
+
+    const tabs = root.querySelectorAll(TAB_SELECTOR);
+
+    tabs.forEach((tab) => {
+      tab.addEventListener('click', () => {
+        tabs.forEach((tabButton) => {
+          tabButton.tabIndex = '0';
+        });
+
+        tab.tabIndex = '-1';
       });
     });
   }
